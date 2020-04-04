db05d5cee9d76f9f291f9b3189f71d0ab4cdcb99
Jarred De Salme <jarred.desalme@gmail.com>
2020-04-04 12:42:23 -0500

    addeda seeds file for sample data and made corrections

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :portfolios
-=======
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
->>>>>>> 915b58f46775d1b588de4082cca52ec384af56d8
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_04_03_025932) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
