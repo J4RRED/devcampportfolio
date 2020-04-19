f4c36bc54a1d14070bca08df1e75f21e223bf387
Jarred De Salme <jarred.desalme@gmail.com>
2020-04-19 15:17:30 -0500

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    J4RREDViewTool::Renderer.copyright 'J4RRED', 'All rights reserved'
+  end
 end
